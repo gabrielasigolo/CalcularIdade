bb58b69f90974ad1ac532e7077f18fd61547b178
Gabriela <gabrielasigolo12@gmail.com>
2021-05-03 15:38:25 -0300

    commit calculo

---
+++ pooCalcularIdade/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalcularIdade
+{
+    public class Pessoa
+    {
+        private String nome; // variavel que guarda o valor 
+
+        // propriedades -- caracteristicas da classe Pessoa
+        public String Nome // propriedade 
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        private int anoNascimento;
+
+        public int AnoNascimento
+        {
+            get { return this.anoNascimento; }
+            set { this.anoNascimento = value; }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Data de nascimento: " + this.AnoNascimento);
+            int idade = this.CalcularIdade();
+            Console.WriteLine("Idade: " + idade);
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime data = DateTime.Now;
+            int ano = data.Year;
+            int idade = ano - this.AnoNascimento;
+            return idade; 
+        }
+
+
+    }
+}
